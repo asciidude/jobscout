2ec5e2a7abe57e5fb6dee08a01f2f6bd0bd7ba47
asciidude <sickspamemail@gmail.com>
2023-03-31 18:20:45 -0500

    init(jobscout): Hello, world :)

---
+++ src/routes/+page.svelte
@@ -0,0 +1,44 @@
+<div class="px-5 py-32 text-center bg-slate-200 dark:bg-slate-900 dark:text-white">
+    <h2 class="font-extrabold text-6xl">
+        Find
+        <span
+            class="
+                text-transparent
+                bg-clip-text
+                bg-gradient-to-r
+                from-pink-400 to-purple-500
+            "
+        >
+            meaningful jobs
+        </span>
+        that match
+        <span
+            class="
+                text-transparent
+                bg-clip-text
+                bg-gradient-to-r
+                from-pink-400 to-purple-500
+                
+            "
+        >
+            your criteria.
+        </span>
+        
+        <div class="my-5"></div>
+
+        <a
+            href="/search"
+            class="
+                text-xl
+                p-3 pr-8 pl-8
+                rounded-full
+                bg-gradient-to-r
+                from-pink-400 to-purple-500
+                text-white
+                font-bold
+            "
+        >
+            Start your search
+        </a>
+    </h2>
+</div>
